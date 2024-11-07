78614debc633eca93beec07fe86b889615332b7c
prakhar17072004 <pmankur2004@gmail.com>
2024-11-07 22:32:19 +0530

    header is updated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import Image from 'next/image';
 import logo from "../assets/logo.png";
 import lotto from "../assets/lotto.png";
+import right_part from "../assets/right_part.png"
 
 const Header = () => {
     return (
-        <div>
+        <div className='flex'>
              {/* Header Section */}
              
-          {/* Right part of header */}
+          {/* Left part of header */}
           <div className=' flex  gap-2 '>
             
-            <Image src={logo} alt="logo" className='mt-[29px] ml-[460px]  h-[48px]'/>
+            <Image src={logo} alt="logo" className='mt-[29px] ml-[300px]  h-[48px]'/>
             <Image src={lotto} alt="lotto" className='h-[20px] mt-[48px] ' />
             
           </div>
+
+        {/* Right part */}
+        <div className='mt-[29px] ml-[400px]'>
+          <Image src={right_part} alt='right_part' />
+        </div>
 
 
         </div>
