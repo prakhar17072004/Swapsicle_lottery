80256204e4d1fc4caada5b1761c75483b9fc094a
prakhar17072004 <pmankur2004@gmail.com>
2024-11-09 17:23:14 +0530

    card2 is updated

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -16,19 +16,26 @@ import rectangle1 from "../assets/Rectangle1.png";
 import rectangle2 from "../assets/Rectangle2.png";
 import Tickets from "../assets/My tickets.png";
 import num2 from "../assets/1.png"
+import Ticket_price from "../assets/Ticket price.png";
+import star from "../assets/star.png";
+import token2 from "../assets/4 ICE.png";
+import total_sold from "../assets/Total sold.png";
+import ticket_count from "../assets/180 tickets.png";
+import rectangle3 from "../assets/Rectangle3.png";
+import buy from "../assets/buy_button.png";
 
 
 const Hero = () => {
     return (
         <div className=''>
-
+             {/* Top section of heor page */}
             <div className='flex mt-[170px]'>
-               
+               {/* left side */}
                <Image src={round} alt="" className=' ml-[400px]'/>
                 <Image src={rectangle1} alt="" className='ml-2 relative'/>
                 <Image src={num} alt="" className='absolute ml-[514px] mt-2'/>
                
-                
+                {/* right side */}
                 <Image src={Tickets} alt="" className='ml-[556px] '/>
                 <Image src={rectangle2} alt="" className='ml-2 relative'/>
                 <Image src={num2} alt="" className='absolute ml-[1200px] mt-2'/>
@@ -50,8 +57,23 @@ const Hero = () => {
                 </div>
                 {/* hero_section_2 */}
 
-                  <div>
+                  <div className='relative'>
                   <Image src={hero_sec_2} className=' ml-2'/>
+                  <Image src={Ticket_price} className='absolute -mt-[230px] ml-10 '/>
+                  <Image src={star} className=' absolute  -mt-[230px]  ml-[460px]'/>
+                  <Image src={logo1} className='absolute -mt-[160px]   ml-10'/>
+                  <Image src={token2} className=' absolute  -mt-[160px] ml-20'/>
+
+                  <Image src={total_sold} className='absolute ml-10  -mt-[80px]'/>
+                  <Image src={ticket_count} className='absolute -mt-[56px] ml-10'/>
+                  <Image src={rectangle3} className='absolute -mt-[88px]  ml-[310px]'/>
+                  <Image src={buy} className='absolute -mt-[84px]  ml-[392px]'/>
+                  <span className='absolute -mt-[72px] ml-[328px] font-bold text-white text-[16px] h-[24px]'>-</span>
+                  <span className='absolute -mt-[72px]  ml-[344px] font-bold text-white text-[16px] h-[24px]'>1</span>
+                  <span className='absolute -mt-[72px] ml-[360px] font-bold text-white text-[16px] h-[24px]'>+</span>
+                  
+
+
                   </div>
             </div>
             <div className='flex mt-4 ml-[400px]'>
