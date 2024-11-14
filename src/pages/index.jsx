b69e824d0ebf289ee1b56a909b875c58177016d0
prakhar17072004 <pmankur2004@gmail.com>
2024-11-14 22:01:41 +0530

    bg ui is updated

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,7 +12,7 @@ const Index = () => {
             {/* background_img */}
             
             <div className='absolute bg-image overflow-y-hidden '>
-                <Image src={Bg} alt="bg_image" className='max-h-[2400px] min-w-[400px] max-w-[1800px]     -mt-[102px]  '/>
+                <Image src={Bg} alt="bg_image" className='min-h-[3600px] min-w-[400px] max-w-[1800px]     -mt-[102px]  '/>
                 </div>
            <div className='absolute max-w-[1260px] min-w-[400px] '>  <Header/></div>
            <div className='absolute  '>  <Hero/></div>
