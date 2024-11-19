6b28cd5e5c18fcc18271dbf1ac58ea88a62f1af1
prakhar17072004 <pmankur2004@gmail.com>
2024-11-19 22:03:47 +0530

    new  ui is of bg is changed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,11 +11,11 @@ const Index = () => {
         <div className='relative ' >
             {/* background_img */}
             
-            <div className='absolute bg-image overflow-y-hidden '>
+            {/* <div className='absolute bg-image overflow-y-hidden '>
                 <Image src={Bg} alt="bg_image" className='min-h-[3600px] min-w-[400px] max-w-[1800px]     -mt-[102px]  '/>
                 </div>
            <div className='absolute max-w-[1260px] min-w-[400px] '>  <Header/></div>
-           <div className='absolute  '>  <Hero/></div>
+           <div className='absolute  '>  <Hero/></div> */}
                
             
 
